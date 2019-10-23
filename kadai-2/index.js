65ce9b3a3e293a0d54feb677b295f8dab8ff5594
natsutakahashi <nats2627@gmail.com>
2019-10-23 05:16:07 +0000

    some fix

---
+++ kadai-2/index.js
@@ -0,0 +1,13 @@
+const firstName = 'Natsu'
+const secondName = 'Takahashi'
+console.log(firstName + secondName)
+
+const sum = 1+2+3+4+5+6+7+8+9+10
+console.log(sum)
+
+const hasDog = false
+const hasCat = true
+
+console.log(hasDog || hasCat)
+console.log(hasDog && hasCat)
+console.log(hasDog && !hasCat)
